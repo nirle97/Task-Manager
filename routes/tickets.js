d34208fbe23ccfe2072fffe0b4f9b84421ccd5e6
Nir Levy <nirle97@gmail.com>
2021-03-23 16:39:02 +0200

    PATCH request done

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,17 +1,40 @@
 const { Router } = require("express");
 const tickets = Router();
 const mongoose = require("mongoose");
+const ObjectId = require('mongodb').ObjectID;
 const Tickets = require("../mongo");
 require('dotenv').config()
 
 tickets.get("/", (req, res) => {
-    Tickets.find().then(tickets => {
-        const searchText = req.query.searchText;
-        const filteredTickets = tickets.filter(ticket => 
-            ticket.title.includes(searchText)
-        )
-        res.send(filteredTickets)
-    })
+    try {
+        Tickets
+        .find()
+        .then(tickets => {
+            const filteredTickets = tickets.filter(ticket => 
+                ticket.title.includes(req.query.searchText)
+            )
+            res.send(filteredTickets)
+        })
+    } catch(e) {
+        console.log(e);
+    }   
   })
+
+tickets.patch("/:ticketId/:status", async (req, res) => {
+    const status = req.params.status === "done" ? true : req.params.status === "undone" 
+    ? false : "invalid"
+    if (status === "invalid") return res.status(404).send({Error: "Invalid status (needs to be done or undone)"})
+    try {
+        const updateDone = req.params.status === "done" ? true : false
+        await Tickets.findOneAndUpdate({"_id": req.params.ticketId}, {"done": status}, {new: true});
+        res.status(200).send({updated: true})
+
+    } catch(e) {
+        console.log(e);
+        res.status(404).send({Error: "Invalid ID"});
+    }
+  })
+
+
 
 module.exports = tickets;
