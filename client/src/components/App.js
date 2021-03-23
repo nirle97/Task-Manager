7f7a8a01235b021234eb12f53c1a2352bdf45a2d
Nir Levy <nirle97@gmail.com>
2021-03-24 01:07:31 +0200

    added search filter

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,34 +5,41 @@ import Ticket from "./Ticket"
 
 function App() {
   const [tickets, setTickets] = useState([])
+  const [ticketsToDisplay, setTicketsToDisplay] = useState([])
 
   useEffect(() => {
+    console.log("useEffect");
     axios
     .get("/api/tickets")
     .then(tickets => {
       setTickets(tickets.data)
+      setTicketsToDisplay(tickets.data)
     })
-  })
+  }, [])
+
+const filterSearch = (e) => {
+  const searchedText = e.target.value;
+  const filterdTickets = tickets.filter(ticket => 
+    ticket.title.includes(searchedText))
+  setTicketsToDisplay(filterdTickets)
+}
+
   return (
     <div className="App">
-    {tickets.map((ticket, i) => {
-      return (
-
-          <Ticket 
-            key={`ticket - ${i}`}
-            title={ticket.title}
-            content={ticket.content}
-            userEmail={ticket.userEmail}
-            done={ticket.done}
-            creationTime={ticket.creationTime}
-            labels={ticket.labels ? ticket.labels: null}
-          />
-        )
-      }
-    )}
-
+    <input id="searchInput" type="text" onChange={filterSearch}></input>
+      {ticketsToDisplay.map((ticket, i) => 
+        <Ticket 
+          key={`ticket - ${i}`}
+          title={ticket.title}
+          content={ticket.content}
+          userEmail={ticket.userEmail}
+          done={ticket.done}
+          creationTime={ticket.creationTime}
+          labels={ticket.labels}
+        />
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -3,8 +3,8 @@ import '../styles/Ticket.css';
 import React, {useState, useEffect} from "react";
 import env from "react-dotenv";
 
-function Ticket({title, content, userEmail, done, creationTime, labels}) {
-    const [allLabels, setAllLabels] = useState([labels])
+function Ticket({title, content, userEmail, creationTime, labels}) {
+    const [allLabels, setAllLabels] = useState(labels)
     return (
         <div className="ticket">
             <h3 className="ticket-title">{title}</h3>
@@ -13,8 +13,7 @@ function Ticket({title, content, userEmail, done, creationTime, labels}) {
                 <p className="user-email">By {userEmail} | </p>
                 <p className="creation-time">{creationTime}</p>
             </span>
-            <span className="label">{allLabels[0].labels}</span>
-            {allLabels.length > 0 &&
+            {labels.length > 0 &&
                 labels.map((label, i) => <span key={`label - ${i}`} className="label">{label} </span>)
             }
         </div>
