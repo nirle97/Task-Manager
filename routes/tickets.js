056daecbb083581ed81b2370d0fe16fc2a21136c
Nir Levy <nirle97@gmail.com>
2021-03-23 16:47:22 +0200

    added favicon

--- routes/tickets.js
+++ routes/tickets.js
@@ -13,19 +13,18 @@ tickets.get("/", (req, res) => {
             const filteredTickets = tickets.filter(ticket => 
                 ticket.title.includes(req.query.searchText)
             )
-            res.send(filteredTickets)
+            res.status(200).send(filteredTickets)
         })
     } catch(e) {
         console.log(e);
+        res.status(404).send({Error: "could not get data from mongodb"});
     }   
   })
 
 tickets.patch("/:ticketId/:status", async (req, res) => {
-    const status = req.params.status === "done" ? true : req.params.status === "undone" 
-    ? false : "invalid"
+    const status = req.params.status === "done" ? true : req.params.status === "undone" ? false : "invalid"
     if (status === "invalid") return res.status(404).send({Error: "Invalid status (needs to be done or undone)"})
     try {
-        const updateDone = req.params.status === "done" ? true : false
         await Tickets.findOneAndUpdate({"_id": req.params.ticketId}, {"done": status}, {new: true});
         res.status(200).send({updated: true})
 
